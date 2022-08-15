67087ca72b6595e89ea1e29ed1dc5b4d77a5fa89
ashan-tharaka <ashantharaka715@gmail.com>
2022-08-15 07:26:38 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,28 +2,19 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Todo from "./components/Todo";
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
-import Todos from './components/Todos';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Todos from "./components/Todos";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Todo/>}/>
-        <Route path="/todos" element={<Todos/>}/>
-        
-
-
-      </Routes>
-
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Todo />} />
+          <Route path="/todos" element={<Todos />} />
+        </Routes>
       </BrowserRouter>
-
-
-      
-    
     </div>
   );
 }
